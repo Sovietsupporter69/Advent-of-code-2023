0b3dec4ec7ee51e8535821347ef39dccea477510
Sovietsupporter69 <84580170+Sovietsupporter69@users.noreply.github.com>
2024-09-23 10:21:28 +0100

    Add project files.

---
+++ Advent-of-code-2023/Program.cs
@@ -0,0 +1,9 @@
+﻿FileStream in1 = File.Open("Input_1.txt", FileMode.Open);
+Console.WriteLine(in1.Length);
+Console.WriteLine(in1.Length);
+for (int i = 0;  i < in1.Length; i++) {
+    string word = in1.ToString();
+    for (int j = 0; j < in1.Length; j++) {
+
+    }
+}
